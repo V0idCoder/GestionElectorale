8b9e0eb387b4cd303591063e165431787ef613a5
Gagnebin Eliot <Eliot.Gagnebin@ceff.ch>
2023-05-26 08:35:23 +0200

    class Commune & class Election

---
+++ gestion_electoraleUI/Commune.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestion_electoraleUI
+{
+    internal class Commune
+    {
+    }
+}
---
+++ gestion_electoraleUI/Election.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestion_electoraleUI
+{
+    public class Election
+    {
+        public int electionId { get; set; }
+        public String nom { get; set; }
+        public DateOnly dateElection { get; set; }
+        public int nbSiege { get; set; }
+        public String Description { get; set; }
+        public int nbBulletin { get; set; }
+        public int nbBulletinNull { get; set; }
+        public int nbBulletinBlanc { get; set; }
+
+        //Héritage de communeId ? --> 
+
+
+    }
+}
